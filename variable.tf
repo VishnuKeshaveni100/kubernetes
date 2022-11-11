f011325a53e79ef93290483a5fc69ae1495f5bba
VishnuKeshaveni100 <109942273+VishnuKeshaveni100@users.noreply.github.com>
2022-11-11 19:52:09 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,19 @@
+ variable "aws_access_key" {
+  type = string
+  default = ""
+ }
+
+ variable "aws_secret_key" {
+  type = string
+  default = ""
+ }
+ 
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-your_first_subnet_id"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-your_second_subnet_id"
+ }
